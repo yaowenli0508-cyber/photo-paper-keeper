981b97737e695e441650472c59313109654c3ae1
yaowenli0508-cyber <yaowenli0508@gmail.com>
2026-08-07 11:38:44 +0800

    修复网页版离线缓存更新

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,9 @@
-const CACHE = "photo-paper-keeper-v3";
-const ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest", "icon.svg"];
-self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
-self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
-self.addEventListener("fetch", (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))));
+const CACHE = "photo-paper-keeper-v4";
+const ASSETS = ["./", "index.html", "styles.css?v=4", "app.js?v=4", "manifest.webmanifest", "icon.svg"];
+self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener("fetch", (event) => event.respondWith(fetch(event.request).then((response) => {
+  const copy = response.clone();
+  caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+  return response;
+}).catch(() => caches.match(event.request).then((cached) => cached || caches.match("./")))));
